6fad8ba3760ef46ddbf6eeb2a9b24ab76e8e736b
Marcus Grant <marcusfg@pm.me>
2025-08-23 20:08:33 +0200

    Ft: Change parse_args to pattern match by arg len
    - Pattern match by args length
    - Default duration to 600s/10m
    - Test for default value
    - Test u32 max value arg parses to same int

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,11 @@
 // src/cli.rs
 
 fn parse_args(args: Vec<String>) -> Option<u32> {
-    // Check if we have exactly 2 arguments (program name + duration)
-    if args.len() == 2 {
-        // Try to parse the 2nd arg as number
-        args[1].parse().ok()
-    }
-    else {
-        None
+    // TODO: Consider using a defaults module or struct for default values
+    match args.len() {
+        1 => Some(600), // Default to 10 minutes if no duration provided
+        2 => args[1].parse::<u32>().ok(), // Parse 2nd argument as u32
+        _ => None, // Invalid number of arguments
     }
 }
 
@@ -15,9 +13,18 @@ fn parse_args(args: Vec<String>) -> Option<u32> {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn parse_args_extracts_duration() {
+    fn parse_args_extracts_second_duration() {
         // Test: prase_args should extract duration from CLI args
         let args = vec!["timeterm".to_string(), "30".to_string()];
         assert_eq!(super::parse_args(args), Some(30));
+        let args = vec!["timeterm".to_string(), "4294967295".to_string()];
+        assert_eq!(super::parse_args(args), Some(4294967295));
+    }
+
+    #[test]
+    fn parse_args_defaults_10min() {
+        // Test: parse_args should default to 10 minutes if no args
+        let args = vec!["timeterm".to_string()];
+        assert_eq!(super::parse_args(args), Some(600));
     }
 }
